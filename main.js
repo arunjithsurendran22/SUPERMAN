5ea767533664b67215032f86d42e5ff4d889a6d2
arunjithsurendran22 <62588065+arunjithsurendran22@users.noreply.github.com>
2023-09-10 09:35:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function clickMe(){
+    var text = document.getElementById('popup');
+        if (text.style.display==='none') {
+            text.style.display='block';
+        } else {
+            text.style.display = 'none';
+        }
+}
+
+function search(){
+    var text = document.getElementById('my-btn');
+        if (text.style.display==='none') {
+            text.style.display='block';
+        } else {
+            text.style.display = 'none';
+        }
+}
